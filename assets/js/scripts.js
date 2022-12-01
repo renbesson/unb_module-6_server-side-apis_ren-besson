432b518c1f202776e044b99afdab8184a71a8a17
Ren Besson <ren.besson@outlook.com>
2022-12-01 00:29:40 -0400

    CSS Done

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,7 +2,7 @@ $(function () {
   var API_KEY = "1914961ce3e50675c3b4e7d5d0a3aa5b";
   var cityInput = $("#city-input");
 
-  //////////////////////////////////////////////////////////////////
+  /////*** Prints one history button per localStorage key ***///////
   var printHistoryBtns = function () {
     var searchColEl = $("#history");
 
@@ -12,12 +12,12 @@ $(function () {
 
     $.each(localStorageKeys, function (index, key) {
       searchColEl.append(
-        `<button class="history-btn" id="${key}">${key}</button>`
+        `<button class="btn mb-2" id="${key}">${key}</button>`
       );
     });
   };
   //////////////////////////////////////////////////////////////////
-  //////////////////////////////////////////////////////////////////
+  /////*** Prints both today and 5-day forecasts ***////////////////
   var printForecast = function (data) {
     var todayEl = $("#today-forecast");
     var fiveDayEl = $("#five-day-forecast");
@@ -42,23 +42,30 @@ $(function () {
 
     $.each(data.list, function (index, value) {
       var card = `
-  <div class="card">
-    <p>${value.dt_txt.substring(0, 10)}</p>
-    <img src="http://openweathermap.org/img/wn/${
+  <div class="card w-60 shadow-xl bg-white m-2">
+    <div class="card-body">
+      <p class="card-title">${value.dt_txt.substring(0, 10)}</p>
+      <p>Temp: ${value.main.temp} C</p>
+      <p>Wind: ${value.wind.speed} Km/h</p>
+      <p>Humidity: ${value.main.humidity} %</p>
+    </div>
+    <figure><img src="http://openweathermap.org/img/wn/${
       value.weather[0].icon
-    }@2x.png"></img>
-    <p>Temp: ${value.main.temp} C</p>
-<p>Wind: ${value.wind.speed} Km/h</p>
-<p>Humidity: ${value.main.humidity} %</p>
+    }@2x.png"></img></figure>
+  </div>
     `;
 
       fiveDayEl.append(card);
     });
   };
   //////////////////////////////////////////////////////////////////
-  //////////////////////////////////////////////////////////////////
+  /////*** Fetch the weather info for the typed city ***////////////
   var getApi = function (city) {
     var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${API_KEY}`;
+    if (!city) {
+      alert("Please type a valid city");
+      return;
+    }
 
     fetch(requestUrl)
       .then(function (response) {
@@ -69,14 +76,15 @@ $(function () {
             printHistoryBtns();
           });
         } else {
-          alert("Error: " + response.statusText);
+          alert("Error: City " + response.statusText);
         }
       })
       .catch(function (error) {
-        alert("Unable to connect to GitHub");
+        alert("Unable to fetch!");
       });
   };
   //////////////////////////////////////////////////////////////////
+  /////*** Starts the application ***///////////////////////////////
 
   var searchBtn = $("#search-btn");
 
